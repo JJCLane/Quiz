04518c9d9a03d6c166bad04c4b92c9a1492437f8
Ryan <deadlyryz@hotmail.co.uk>
2015-11-07 17:23:09 +0000

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html ng-app="todoApp">
     <head>
         <title>Laravel</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link href="/css/app.css" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        <form>
+            <select>
+                <option value="Single choice question">Single choice question</option>
+                <option value="Multiple choice question">Multiple choice question</option>
+            </select>
+            <div>
+                <input type="text" ng-model="yourName" placeholder="Enter a question here">
+                <input type="text" ng-model="yourName" placeholder="Enter a question here">
             </div>
-        </div>
+            <div>
+                <input type="text" ng-model="yourName" placeholder="Enter a question here">
+                <input type="text" ng-model="yourName" placeholder="Enter a question here">
+            </div>
+            
+        </form>
+        <div ng-controller="TodoListController as todoList">
+          <ul class="unstyled">
+            <li ng-repeat="todo in todoList.todos">
+              <input type="checkbox" ng-model="todo.done">
+              <span class="done-@{{todo.done}}">@{{todo.text}}</span>
+          </li>
+      </ul>
+      <form ng-submit="todoList.addTodo()">
+        <input type="text" ng-model="todoList.todoText"  size="30"
+        placeholder="add new todo here">
+        <input class="btn-primary" type="submit" value="add">
+    </form>
+</div>
+        <h1>Hello @{{yourName}}!</h1>
+        <script src="/js/all.js"></script>
     </body>
 </html>
