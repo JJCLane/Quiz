ed2a79a0ae0b7e7e5f4b7e9b9195c09ce3788bd1
Ryan <deadlyryz@hotmail.co.uk>
2015-11-07 17:39:35 +0000

    masterpage

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html ng-app="todoApp">
+    <head>
+        <title>Quiz</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <div>	
+        @yield("content")
+        </div>
+        <script src="/js/all.js"></script>
+    </body>
+</html>
