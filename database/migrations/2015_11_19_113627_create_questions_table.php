5f2609a158dd585fdc78b4e0fca961e573704f6f
Jordan Lane <jordan.lane@steinias.com>
2015-11-19 12:55:59 +0000

    Add basic models and migrations

---
+++ database/migrations/2015_11_19_113627_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quiz_id')->index();
+            $table->integer('question_type_id')->index();
+            $table->string('title');
+            $table->integer('time_limit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
